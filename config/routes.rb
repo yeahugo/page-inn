17ca3efaa3168240b216e2155fca0041a85db685
Ye Jiahuan <yjhuestc@yahoo.com.cn>
2012-03-09 18:31:18 +0800

    修改一些测试用例

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Page::Application.routes.draw do
-  #resources :book_userships
+  resources :book_userships
 
-  devise_for :users
+  devise_for :users ,:path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
 
   resources :users do
     member do
